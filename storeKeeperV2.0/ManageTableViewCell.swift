1cddf5a651ebaad693fdaef3f29ca209559f2e57
Nikita Gvozdikov <pounddnuop@mail.ru>
2020-11-08 19:19:55 +0300

    The first storyBoard

---
+++ storeKeeperV2.0/ManageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ManageTableViewCell.swift
+//  storeKeeperV2.0
+//
+//  Created by Никита Гвоздиков on 08.11.2020.
+//
+
+import UIKit
+
+class ManageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
