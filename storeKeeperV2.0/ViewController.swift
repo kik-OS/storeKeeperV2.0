dade0866a516b12ad46592f39a0c8d75de07c128
Nikita Gvozdikov <pounddnuop@mail.ru>
2020-11-08 18:57:46 +0300

    Initial Commit

---
+++ storeKeeperV2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storeKeeperV2.0
+//
+//  Created by Никита Гвоздиков on 08.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
